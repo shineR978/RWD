46f7e26019373274829e197f75dbed3818b985f2
DESKTOP-8PTGRH7\User <bw812810@gmail.com>
2024-09-15 22:05:04 +0800

    修改漢堡初期錯誤

--- project/app.js
+++ project/app.js
@@ -1,17 +1,1 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const openMenuButton = document.querySelector(".open-menu");
-//   const closeMenuButton = document.querySelector(".close-menu");
-//   const nav = document.querySelector("nav");
 
-//   openMenuButton.addEventListener("click", function () {
-//     nav.style.right = "0";
-//     openMenuButton.style.display = "none";
-//     closeMenuButton.style.display = "block";
-//   });
-
-//   closeMenuButton.addEventListener("click", function () {
-//     nav.style.right = "-100%";
-//     openMenuButton.style.display = "block";
-//     closeMenuButton.style.display = "none";
-//   });
-// });
